1752f4454914faf5c389631301269a4a04ad0b9d
root676 <clemens.raffler@gmx.at>
2017-07-30 19:57:28 +0200

    script cleanup

--- scripts/alg_iso_multiple.py
+++ scripts/alg_iso_multiple.py
@@ -1,3 +1,10 @@
+
+##input_Network=vector
+##input_Points=vector
+##input_cost_type=string
+##input_iso_steps=string
+##output_isochrones=output vector
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
