82e4de752313e90f71b0e559a07319ad0f9e632a
root676 <clemens.raffler@gmx.at>
2017-11-09 20:24:38 +0100

    add QneatUitilities and new constructor signatures

---
+++ scripts/QneatUtilities.py
@@ -0,0 +1,93 @@
+
+from qgis.core import *
+from qgis.analysis import *
+from qgis.networkanalysis import *
+
+from PyQt4.QtCore import QVariant
+
+def AssignAnalysisCrs(input_network):
+    log("Setting analysis CRS")
+    AnalysisCrs = input_network.crs()
+
+def log(message):
+    QgsMessageLog.LogMessage(message)
+    
+def populateMemoryQgsVectorLayer(string_geomtype, string_layername, crs, list_geometry, list_qgsfield):
+    
+    #create new vector layer from self.crs
+    vector_layer = QgsVectorLayer(string_geomtype, string_layername, "memory")
+    
+    #set crs from class
+    vector_layer.setCrs(crs)
+    
+    #set fields
+    provider = vector_layer.dataProvider()
+    provider.addAttributes(list_qgsfield) #[QgsField('fid',QVariant.Int),QgsField("origin_point_id", QVariant.Double),QgsField("iso", QVariant.Int)]
+    vector_layer.updateFields()
+    
+    #fill layer with geom and attrs
+    vector_layer.startEditing()
+    for i, geometry in enumerate(list_geometry):
+        feat = QgsFeature()
+        feat.setGeometry(geometry)#geometry from point
+        feat.setAttributes(list_qgsfield[i])
+        vector_layer.addFeature(feat, True)
+    vector_layer.commitChanges()
+
+    return vector_layer
+
+def getLayerGeometryType(vlayer):
+    wkbType = vlayer.wkbType()
+    try:
+        if wkbType in (QGis.WKBPoint, QGis.WKBMultiPoint,
+                       QGis.WKBPoint25D, QGis.WKBMultiPoint25D):
+            return QGis.WKBPoint
+        elif wkbType in (QGis.WKBLineString, QGis.WKBMultiLineString,
+                         QGis.WKBMultiLineString25D,
+                         QGis.WKBLineString25D):
+
+            return QGis.WKBLineString
+        elif wkbType in (QGis.WKBPolygon, QGis.WKBMultiPolygon,
+                         QGis.WKBMultiPolygon25D, QGis.WKBPolygon25D):
+
+            return QGis.WKBPolygon
+        else:
+            return QGis.WKBUnknown
+    except:
+        return None
+    
+def extractGeometryAsSingle(geom): #must be called per multigeometry object
+    multiGeom = QgsGeometry()
+    list_singleGeometries = []
+    if geom.type() == QGis.Point:
+        if geom.isMultipart():
+            multiGeom = geom.asMultiPoint()
+            for i in multiGeom:
+                list_singleGeometries.append(QgsGeometry().fromPoint(i))
+        else:
+            list_singleGeometries.append(geom)
+    elif geom.type() == QGis.Line:
+        if geom.isMultipart():
+            multiGeom = geom.asMultiPolyline()
+            for i in multiGeom:
+                list_singleGeometries.append(QgsGeometry().fromPolyline(i))
+        else:
+            list_singleGeometries.append(geom)
+    elif geom.type() == QGis.Polygon:
+        if geom.isMultipart():
+            multiGeom = geom.asMultiPolygon()
+            for i in multiGeom:
+                list_singleGeometries.append(QgsGeometry().fromPolygon(i))
+        else:
+            list_singleGeometries.append(geom)
+    return list_singleGeometries
+
+def getMultiGeometryAsList(vlayer):
+    list_geometry = [feature.geometry() for feature in vlayer]
+    return list_geometry
+
+
+def getSingleGeometryAsList(vlayer):
+    list_multigeom = getMultiGeometryAsList(vlayer)
+    for geom in list_multigeom:
+        
