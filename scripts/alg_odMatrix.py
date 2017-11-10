9d14ff60849fe4bc8f4d8b1d3a49e0d112d7d28e
root676 <clemens.raffler@gmx.at>
2017-11-10 23:45:44 +0100

    update testcase

--- scripts/alg_odMatrix.py
+++ scripts/alg_odMatrix.py
@@ -1,3 +1,4 @@
+##QNEAT tools=group
 ##input_network=vector
 ##input_points=vector
 #input_directionFieldId= optional field
@@ -5,10 +6,13 @@
 #input_reverseDirectionNumber=optional number
 #input_bothDirectionValue=optional number
 #input_defaultDirection=optional number 3
-##output_matrix=output table
+#output_matrix=output table
 
 import QNEAT.framework.QneatUtilities as util
+import QNEAT.framework.QneatFramework as QneatFramework
 
 util.log("HelloWorld")
 util.log("This is the QNEAT Plugin speaking!")
+
+
 
